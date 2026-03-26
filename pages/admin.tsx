3ef81544bac58d09798292a4e1447aa7ec4d6451
dusousas <dusousadeveloper@gmail.com>
2026-03-25 23:21:03 -0300

    admin permissom

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,6 +2,9 @@ import AdminDashboard from "@/components/Admin/AdminDashboard";
 import { GetServerSideProps } from "next";
 import { getAdminDashboardData } from "@/lib/dashboard-data";
 import { AdminDashboardData } from "@/lib/types";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { isAdminEmail } from "@/lib/admin";
 
 type AdminProps = {
   data: AdminDashboardData;
@@ -11,7 +14,27 @@ export default function Admin({ data }: AdminProps) {
   return <AdminDashboard data={data} />;
 }
 
-export const getServerSideProps: GetServerSideProps<AdminProps> = async () => {
+export const getServerSideProps: GetServerSideProps<AdminProps> = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session?.user?.email) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  if (!isAdminEmail(session.user.email)) {
+    return {
+      redirect: {
+        destination: "/painel",
+        permanent: false,
+      },
+    };
+  }
+
   const data = await getAdminDashboardData();
 
   return {
